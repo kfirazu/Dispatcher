187bece14e5ecdeb930549649311dfbe5fc9cec2
Kfir Azulay <kfira@moveohls.com>
2023-07-05 10:37:34 +0300

    App header basic style

---
+++ src/components/App-Header/app-header.style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+
+
+const StyledContainer = styled.div`
+max-width: 1920px;
+width: 100%;
+height: 74px;
+display: flex;
+align-items: center;
+background-color: #262146;
+padding-right; 20px;
+padding-left: 20px;
+border: 1px;
+
+`
+
+export default StyledContainer
+
+export const StyledInputWrapper = styled.div`
+display: flex;
+`
+
+// background-color: ${({ theme: { palette } }) => palette.primary.main};
