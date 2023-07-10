0d266202dec859b90499c36dabd93285947121c5
Kfir Azulay <kfira@moveohls.com>
2023-07-10 14:25:02 +0300

    feature - filter cmp style

---
+++ src/components/Filter/filter.tsx
@@ -0,0 +1,53 @@
+import CustomDropdown from "../Dropdown/custom-dropdown"
+import { StyledContainer } from "./filter.style"
+
+
+const Filter = () => {
+
+    const categories = [
+        { value: 'business', name: 'Business' },
+        { value: 'entertainment', name: 'Entertainment' },
+        { value: 'general', name: 'General' },
+        { value: 'health', name: 'Health' },
+        { value: 'science', name: 'Science' },
+        { value: 'sports', name: 'Sports' },
+        { value: 'technology', name: 'Technology' }
+
+    ]
+
+    const sources = [
+        { value: 'וואלה', name: 'וואלה! חדשות' }, { value: 'וואלה', name: 'וואלה! תרבות' },
+        { value: "הארץ", name: 'הארץ' }, { value: 'ספורט 5', name: 'ספורט 5' },
+        { value: '"ynet ידיעות אחרונות"', name: '"ynet ידיעות אחרונות"' }, { value: '"כלכליסט"', name: '"כלכליסט"' },
+        { value: '"mako"', name: '"mako"' }, { value: '"TheMarker"', name: '"TheMarker"' },
+        { value: '"ערוץ 13"', name: '"ערוץ 13"' }, { value: '"מעריב און ליין"', name: '"מעריב און ליין"' },
+
+    ]
+
+    const countries = [
+        { value: 'ar', name: 'Argentina' }, { value: 'au', name: 'Austrailia' },
+        { value: 'at', name: 'Austria' }, { value: 'be', name: 'Belgium' },
+        { value: 'br', name: 'Brazil' }, { value: 'bg', name: 'Bulgaria' },
+        { value: 'ca', name: 'Canada' }, { value: 'cn', name: 'China' },
+        { value: 'co', name: 'Colombia' }, { value: 'cu', name: 'Cuba' },
+        { value: 'cz', name: 'Czech Republic' }, { value: 'eg', name: 'Egypt' },
+        { value: 'fr', name: 'France' }, { value: 'de', name: 'Germany' },
+        { value: 'gr', name: 'Greece' }, { value: 'hk', name: 'Hong Kong' },
+        { value: 'hu', name: 'Hungary' }, { value: 'in', name: 'India' },
+        { value: 'id', name: 'Indonesia' }, { value: 'ie', name: 'Ireland' },
+        { value: 'ik', name: 'Israel' }, { value: 'it', name: 'Italy' }
+
+    ]
+
+    return (
+        <StyledContainer>
+            <CustomDropdown children={countries} label={'Country'} placeholder="Country"/>
+            <CustomDropdown children={categories} label={'Category'} placeholder="Catrgory" />
+            <CustomDropdown children={sources} label={'Soruce'} placeholder="Source"/>
+
+        </StyledContainer>
+    )
+
+}
+
+export default Filter
