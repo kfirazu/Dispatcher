a184417f7d95402d719c819844dc130ac21bb27b
Kfir Azulay <kfira@moveohls.com>
2023-07-17 14:48:59 +0300

    FIX: bug fix and cleanup

---
+++ src/components/DateSelector/date-selector.tsx
@@ -0,0 +1,33 @@
+
+import { DatePicker } from '@mui/x-date-pickers'
+import type { } from '@mui/x-date-pickers/themeAugmentation'
+import { useEffect, useState } from 'react'
+import { styledDatePickerSX } from './date-selector.style'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
+
+
+const DateSelector = () => {
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+
+            <DatePicker
+                label='Dates'
+                onChange={(newValue: Date | null) => { setSelectedDate(newValue) }}
+                sx={styledDatePickerSX}
+                showDaysOutsideCurrentMonth
+                slots={{
+                    openPickerIcon: CalendarMonthIcon,
+                    
+                }}
+                  
+            />
+        </LocalizationProvider>
+    )
+}
+
+export default DateSelector
