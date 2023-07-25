a1b7c223170db51d72a7491d6c6478481ac296c2
Kfir Azulay <kfira@moveohls.com>
2023-07-25 17:49:00 +0300

    Installed react query

--- src/App.tsx
+++ src/App.tsx
@@ -6,67 +6,82 @@ import SortBar from './components/Sort-Bar/sort-bar'
 import FeedList from './components/FeedList/feed-list'
 import newsData from './data/news.json'
 import Dashboard from './components/Dashboard/dashboard'
-// import { newsService } from './services/news.service'
-// import { filterByInterface } from './models/filter-by-interface'
 import { Article } from './models/article-interface'
 import { AppContainer, MainContainer } from './styles/global-styles';
 import useIsMobile from './hooks/useIsMobile';
 import FilterBar from './components/Filter/filter-bar';
-import { store } from './store/store';
-import { Provider } from 'react-redux'
+import { useAppDispatch, useAppSelector } from './store/hooks.store';
+import { newsService } from './services/news.service';
+import { useQuery, QueryClient, useQueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { UseIsTablet } from './hooks/useIsTablet';
+import { setInitialArticleList } from './store/news/news.reducer';
+
 
 function App() {
 
+  const isMobile = useIsMobile()
+  const isTablet = UseIsTablet()
+  const dispatch = useAppDispatch()
+  const IsEverything = useAppSelector(state => state.system.isEverything)
+
   //Temporary to fetch mock data
   const [articleList, setArticleList] = useState<Article[]>([])
-  const [IsEverything, setIsEverything] = useState(true)
-  const [isSideBarOpen, setIsSideBarOpen] = useState(true)
-  const isMobile = useIsMobile()
+  const [news, setNews] = useState<any>({})
+
+  const initArticle = useAppSelector(state => state.news.articleList)
 
   // Fetch Data
   // useEffect(() => {
-  //   ; (async (filterBy) => {
+  //   ; (async () => {
   //     try {
-  //       const newsFeed = await newsService.query(filterBy)
+  //       const newsFeed = await newsService.query()
   //       setNews(newsFeed)
+
   //     } catch (err) {
   //       console.log('err from cmp', err)
   //     }
   //   })()
   // }, [])
 
+  // Should load the data when the app is mounting and then put it in redux. 
+  // redux should hold the articleListToDisplay
+
+
+  // useEffect(() => {
+  //   const fetchInitialData = async () => {
+  //     const initialArticles = await newsService.fetchInitialArticles()
+  //     dispatch(setInitialArticleList(initialArticles))
+  //   }
+  //   setArticleList(newsData.articles.slice(0, 10))
+  //   fetchInitialData()
+  // }, [])
+
+  // Fetch initial articles using react-query
+  const { data: initialArticles } = useQuery<Article[]>(["initialArticles"], newsService.fetchInitialArticles, {
+    staleTime: 300000, // 5 minutes (how long the data is considered fresh)
+  });
+
+  // Set the initial articles to Redux store
   useEffect(() => {
-    setArticleList(newsData.articles.slice(0, 10))
-  }, [])
-
-  const [filterBy, setFilterBy] = useState({
-    type: '',
-    country: '',
-    source: '',
-    category: '',
-    keyword: ''
-  })
-
-  const onSetFilterBy = (newFilterBy: any) => {
-    setFilterBy(newFilterBy)
-  }
-
-  const onOpenSideBar = () => {
-    setIsSideBarOpen(true)
-  }
+    if (initialArticles) {
+      setArticleList(initialArticles)
+      dispatch(setInitialArticleList(initialArticles))
+    }
+  }, [dispatch, initialArticles]);
 
   return (
     <>
-      <Provider store={store}>
+      {/* <QueryClientProvider client={queryClient}> */}
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <AppContainer>
+          <AppContainer isMobile={isMobile} isTablet={isTablet}>
             <AppHeader />
+            {/* <MyComponent /> */}
             {IsEverything ?
-              <SortBar onOpenSideBar={onOpenSideBar} />
-              : <FilterBar onOpenSideBar={onOpenSideBar} />
+              <SortBar />
+              : <FilterBar />
             }
-            <MainContainer isMobile={isMobile}>
+            <MainContainer isMobile={isMobile} isTablet={isTablet}>
               <div style={{ borderTop: '1px solid #D9DBE9', display: 'flex' }}>
                 <FeedList articleList={articleList} />
                 <Dashboard articleList={articleList} />
@@ -74,7 +89,8 @@ function App() {
             </MainContainer>
           </AppContainer>
         </ThemeProvider>
-      </Provider>
+      {/* </QueryClientProvider> */}
+
     </>
   )
 }
@@ -82,3 +98,26 @@ function App() {
 
 
 export default App
+
+// export const MyComponent = () => {
+
+//   const { data: articles, isLoading } = useQuery({
+//     queryKey: ['initialArticles'],
+//     queryFn: newsService.fetchInitialArticles
+//   });
+
+//   console.log('articlesssss',articles)
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   } else {
+//     return (
+//       <div>
+//         {/* Your component content */}
+//         {articles.map((article: any) => {
+//           <div>{article.source}</div>
+//         })}
+//       </div>
+//     );
+//   }
+// }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { store } from './store/store';
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+
+    </QueryClientProvider>,
+
+  </Provider>
+
 )
